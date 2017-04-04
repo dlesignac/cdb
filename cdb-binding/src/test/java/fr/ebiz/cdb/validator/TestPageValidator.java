bac5fabe13fccf7b14f6f79299e5b099ebfe2c61
dlesignac <dlesignac@e-biz.fr>
2017-04-04 11:26:07 +0200

    added some unit tests

---
+++ cdb-binding/src/test/java/fr/ebiz/cdb/validator/TestPageValidator.java
@@ -0,0 +1,78 @@
+package fr.ebiz.cdb.validator;
+
+import fr.ebiz.cdb.binding.ComputerPageRequest;
+import fr.ebiz.cdb.core.Column;
+import fr.ebiz.cdb.core.Order;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TestPageValidator {
+    private ComputerPageRequest computerPageDTO;
+
+    @Before
+    public void init() {
+        computerPageDTO = new ComputerPageRequest();
+        computerPageDTO.setFilter("");
+        computerPageDTO.setSort(Column.COMPUTER_NAME);
+        computerPageDTO.setOrder(Order.ASC);
+        computerPageDTO.setLimit(10);
+        computerPageDTO.setPage(1);
+    }
+
+    @Test
+    public void testFilterNull() {
+        computerPageDTO.setFilter(null);
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(true));
+    }
+
+    @Test
+    public void testFilterEmpty() {
+        computerPageDTO.setFilter("");
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(true));
+    }
+
+    @Test
+    public void testSortNull() {
+        computerPageDTO.setSort(null);
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(false));
+    }
+
+    @Test
+    public void testOrderNull() {
+        computerPageDTO.setOrder(null);
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(false));
+    }
+
+    @Test
+    public void testLimitLessThanMin() {
+        computerPageDTO.setLimit(0);
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(false));
+    }
+
+    @Test
+    public void testLimitMoreThanMax() {
+        computerPageDTO.setLimit(101);
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(false));
+    }
+
+    @Test
+    public void testNumberLessThanMin() {
+        computerPageDTO.setPage(0);
+        boolean valid = PageValidator.validate(computerPageDTO).isEmpty();
+        assertThat(valid, is(false));
+    }
+
+    @Test
+    public void testInstantiate() {
+        new PageValidator();
+    }
+}
